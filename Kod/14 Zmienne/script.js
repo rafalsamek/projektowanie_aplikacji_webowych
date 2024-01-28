acf222dec79019795fa3a3832a46ae38ae789ecf
rsamek <rafal.samek@gmail.com>
2024-01-28 10:01:47 +0100

    JS Zmienne

---
+++ Kod/14 Zmienne/script.js
@@ -0,0 +1,27 @@
+let numericVal = -5;
+let stringVal = 'Wartość' // "Wartość"
+let booleanVal = false;
+
+console.log(numericVal);
+console.log(stringVal);
+console.log(booleanVal);
+
+console.log('--------------------------------------------');
+
+console.log(typeof numericVal);
+console.log(typeof stringVal);
+console.log(typeof booleanVal);
+
+console.log('--------------------------------------------');
+
+numericVal = 'Nowa wartość';
+console.log(numericVal);
+console.log(typeof numericVal);
+
+console.log('--------------------------------------------');
+
+let sum = '1' + 1;
+console.log(sum);
+
+let diff = '1' - 1;
+console.log(diff);
